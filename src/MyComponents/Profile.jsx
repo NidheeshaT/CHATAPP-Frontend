783b2acdca304634c31fe5171261bf9793872865
NidheeshaT <ntbayari@gmail.com>
2022-06-18 15:23:44 +0530

    added profile component

---
+++ src/MyComponents/Profile.jsx
@@ -0,0 +1,20 @@
+import "./Profile.css"
+
+function Profile(){
+  return (
+    <>
+      <section id="profile-page">
+          <div id="img"></div>
+          <h3>Hello</h3>
+		  <div><span>Description:</span>Hey There hanging about with WALKIETALKIE</div>
+		  <div id="cont">
+		  	<span>Usage info:</span>
+			<div  id="info"></div>
+		  </div>
+      </section>
+
+    </>
+  )
+}
+
+export default Profile;
